474d4ae0368bd1df88c9969dd5a4192f11e3ecb3
venkatesh0104 <58542245+venkatesh0104@users.noreply.github.com>
2022-07-19 11:24:31 +0530

    Add files via upload
    Added ConsoleAppValueObjectPOC

---
+++ ConsoleAppValueObjectPOC/ConsoleAppValueObjectPOC/Address.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppValueObjectPOC
+{
+    public class Address : ValueObject
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string ZipCode { get; set; }
+
+        private Address() { }
+
+        public Address(string street, string city, string state, string country, string zipcode)
+        {
+            Street = street;
+            City = city;
+            State = state;
+            Country = country;
+            ZipCode = zipcode;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            // Using a yield return statement to return each element one at a time
+            yield return Street;
+            yield return City;
+            yield return State;
+            yield return Country;
+            yield return ZipCode;
+        }
+    }
+}
---
+++ ConsoleAppValueObjectPOC/ConsoleAppValueObjectPOC/Program.cs
@@ -0,0 +1,49 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+//using ConsoleAppValueObjectPOC;
+
+//var one = new Address("Microsoft Way", "Redmond", "WA", "US", "98052");
+//var two = new Address("Microsoft Way", "Redmond", "WA", "US", "98052");
+
+//Console.WriteLine(EqualityComparer<Address>.Default.Equals(one, two)); // True
+//Console.WriteLine(object.Equals(one, two)); // True
+//Console.WriteLine(one.Equals(two)); // True
+//Console.WriteLine(one == two); // True
+
+//int[] a = new int[10];
+//a = MyFunc(2, 10);
+
+
+
+//for (int i = 0; i < a.Length; i++)
+//{
+//    Console.WriteLine(a[i]);
+//}
+//Console.Read();
+
+
+//static int[] MyFunc(int start, int number)
+//{
+//    int[] _number = new int[number];
+//    for (int i = 0; i < number; i++)
+//    {
+//        _number[i] = start + 2 * i;
+//    }
+//    return _number;
+//}
+
+foreach(var item in MyFunc(2, 10))
+{
+    Console.WriteLine(item);
+}
+
+Console.Read();
+
+static IEnumerable<int> MyFunc(int start, int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        yield return start + 2 * i;
+    }
+}
+    
