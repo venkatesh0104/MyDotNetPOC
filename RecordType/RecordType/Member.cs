b7b2c9668a7a7476edd34d39d8674ba5e97fdd61
venkatesh0104 <suravaramvenkatesh@gmail.com>
2022-07-19 15:49:53 +0530

    added RecordType project

---
+++ RecordType/RecordType/Member.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordType
+{
+    public record Member
+    {
+  
+        public int Id { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Address { get; init; }
+    }
+}
---
+++ RecordType/RecordType/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using RecordType;
+
+var memberObj = new Member
+{
+    Id = 1,
+    FirstName = "Suravaram",
+    LastName = "Venkateshwar Reddy",
+    Address = "Ameenpur"
+};
+
+
+var newMember = memberObj with { Address = "Mumbai" };
+
+Console.WriteLine("****************START - Member Details***********");
+
+Console.WriteLine("ID :- " + memberObj.Id);
+Console.WriteLine("FirstName :- " + memberObj.FirstName);
+Console.WriteLine("LastName :- " + memberObj.LastName);
+Console.WriteLine("Address:- " + memberObj.Address);
+Console.ReadLine();
+
+Console.WriteLine("****************END - Member Details***********");
